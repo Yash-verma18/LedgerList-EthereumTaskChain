23f259385ba510f9ae71aac518fc513948793d42
recognizeyourself <recognizeyourself909@gmail.com>
2023-08-27 01:10:32 +0530

    Creates task + test cases

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,6 +1,29 @@
 pragma solidity ^0.5.0;
 
 contract TodoList {
-     uint public taskCount = 0;
- 
+    uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    event TaskCreated(uint id, string content, bool completed);
+
+    constructor() public {
+        createTask("Check out dappuniversity.com");
+    }
+
+    /* 
+    solidity allows us to create events that are triggered anytime you know something happens inside of a smart contract and external consumers can subscribe to these events to know uh whenever the event happened and events are really useful because you know whatever we call this create task function we don't always know when you know the transaction actually completed we don't always know when it was mined and things like that and it can be really useful to listen to those events uh in order to you know know that it was finished so we can create an event in solidity before we call it here we need to actually declare it inside of our smart contract we'll just do it like this we'll go below this mapping we'll say event we'll say task created and notice that's capitalized all right here's a semicolon here and we'll just add some arguments to this event we'll say uint id this will be the id the task that was created a string this is the content and the completed property so boolean completed all right so that's how we create an event inside of solidity right this just means that you know the task created event is available to us inside of the smart contract and i'll show you how we can actually call it we do like this we use the emit keyword emit task created and we pass in the arguments so the id here is the task count and the content which is passed in from the function and false because it's a new task and we haven't completed it yet and that's pretty easy that's how you trigger events inside of solidity and we can subscribe to these events you know inside the client-side application or you know anywhere that can listen to events on a smart contract all right while we're here i'm going to go ahead and actually write the tests for creating the to-do item so i'll open the test file over here do*/
+
+    // creat a function to create a tasks
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+        emit TaskCreated(taskCount, _content, false);
+    }
 }
---
+++ src/app.js
@@ -0,0 +1,151 @@
+App = {
+  loading: false,
+  contracts: {},
+
+  load: async () => {
+    await App.loadWeb3();
+    await App.loadAccount();
+    await App.loadContract();
+    await App.render();
+  },
+
+  // https://medium.com/metamask/https-medium-com-metamask-breaking-change-injecting-web3-7722797916a8
+  loadWeb3: async () => {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      try {
+        // Request account access if needed
+        await window.ethereum.request({ method: "eth_requestAccounts" });
+      } catch (error) {
+        console.error("User denied account access");
+      }
+    }
+    // Legacy dapp browsers
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    }
+    // Non-dapp browsers
+    else {
+      console.log(
+        "Non-Ethereum browser detected. You should consider trying MetaMask!"
+      );
+    }
+  },
+
+  loadAccount: async () => {
+    // Set the current blockchain account
+
+    App.account = await web3.eth.getAccounts();
+    App.account = App.account[0];
+    console.log("app.account", App.account);
+  },
+
+  loadContract: async () => {
+    // Create a JavaScript version of the smart contract
+    const todoList = await $.getJSON("TodoList.json");
+
+    App.contracts.TodoList = await TruffleContract(todoList);
+    App.contracts.TodoList.setProvider(window.web3.currentProvider);
+    console.log("todoList", todoList);
+
+    // Hydrate the smart contract with values from the blockchain
+    App.todoList = await App.contracts.TodoList.deployed();
+  },
+
+  render: async () => {
+    // Prevent double render
+    if (App.loading) {
+      return;
+    }
+
+    try {
+      // Update app loading state
+      App.setLoading(true);
+
+      // Render Account
+      $("#account").html(App.account);
+
+      // Render Tasks
+      await App.renderTasks();
+
+      // App.setLoading(false);
+    } catch (error) {
+      console.error("Error during rendering:", error);
+    } finally {
+      // Update loading state
+      App.setLoading(false);
+    }
+  },
+
+  createTask: async () => {
+    App.setLoading(true);
+    const content = $("#newTask").val();
+    console.log("content", content);
+
+    try {
+      await App.todoList.createTask(content, { from: App.account });
+    } catch (error) {
+      console.error("Error during creating task:", error);
+    } finally {
+      App.setLoading(false);
+    }
+
+    window.location.reload();
+  },
+
+  setLoading: (boolean) => {
+    App.loading = boolean;
+    const loader = $("#loader");
+    const content = $("#content");
+    if (boolean) {
+      loader.show();
+      content.hide();
+    } else {
+      loader.hide();
+      content.show();
+    }
+  },
+
+  renderTasks: async () => {
+    // Load the total task count from the blockchain
+    const taskCount = await App.todoList.taskCount();
+    console.log("taskCount", taskCount.toNumber());
+    const $taskTemplate = $(".taskTemplate");
+
+    // Render out each task with a new task template
+    for (var i = 1; i <= taskCount; i++) {
+      // Fetch the task data from the blockchain
+      const task = await App.todoList.tasks(i);
+      const taskId = task[0].toNumber();
+      const taskContent = task[1];
+      const taskCompleted = task[2];
+
+      console.log("task", task);
+
+      // Create the html for the task
+      const $newTaskTemplate = $taskTemplate.clone();
+      $newTaskTemplate.find(".content").html(taskContent);
+      $newTaskTemplate
+        .find("input")
+        .prop("name", taskId)
+        .prop("checked", taskCompleted);
+      // .on("click", App.toggleCompleted);
+
+      // Put the task in the correct list
+      if (taskCompleted) {
+        $("#completedTaskList").append($newTaskTemplate);
+      } else {
+        $("#taskList").append($newTaskTemplate);
+      }
+
+      // Show the task
+      $newTaskTemplate.show();
+    }
+  },
+};
+
+$(() => {
+  $(window).load(() => {
+    App.load();
+  });
+});
